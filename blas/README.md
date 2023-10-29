07e0d26bd38992c7a4b56fa444e2ccd2985e2e72
Jean-Michel Friedt <friedtj@free.fr>
2023-10-29 18:54:49 +0100

    matrix inverse and pseudo-inverse demonstations

--- blas/README.md
+++ blas/README.md
@@ -77,10 +77,24 @@ Executing the C++ program ``demo4_matrix_rectangleRMcomplex`` using complex argu
 3.00+j2.00 2.00+j5.00 1.00+j8.00                                        3 +  2i    2 +  5i    1 +  8i
 ```
 
-Finally the last example, relying on the knowledge accumulated so far about matrix orientation and data organization, is ``demo_matrix_final.cpp`` using
+Finally the last example, relying on the knowledge accumulated so far about matrix orientation and data organization, is ``demo6_matrix_xcorr.cpp`` using
 complex number with time delayed copies of a code in a noisy signal.
 
 *When correlating, make sure to transpose with the complex conjugate* and not a simple line to column transposition. Only by conjugating will the power
 accumulate in the correlation peak:
 
 <img src="demo_xcorr.png">
+
+Knowing that a reference signal lies in the noisy record is fine as the output of the cross-correlation, but maybe we want to know the
+weight of this signa contibution to later cancel and recover the underlying useful message (case of jamming or spoofing attacks). This 
+result is achieved with the pseudo inverse of the time delayed copies of the record. Indeed assuming a noisy signal $s$ has been recorded
+on one antenna, then the time delayed copies $x$ of $s$ observed on another antenna can be expressed as a linear combination of the elements
+of matrix $X$ including time delayed copies of $s$ as columns. Thus $x=w.X$ and the weights $w$ are the solution of $w=x.X^{-1}$. However
+if $X$ is not square as is the case when many more observations (lines) than delays (columns) have been collected, then $X^{-1}$ does not
+exist but is computed as the *pseudo-inverse* $pinv(X)=X(X^tX)^{-1}$: indeed since $X^tX$ is square, we can compute its inverse and multiply
+by the initial $X$. Finally, the weights $w$ are $w=x.pinv(X)$. First we check we understand how to compute the inverse of a square
+matrix in demo7_matrix_inv.cpp through its LU decomposition, and we apply the knowledge in demo8_matrix_pinv.cpp where three weighted delayed
+copies of the signal have been added to noise:
+
+<img src="demo_pinv.png">
+
---
+++ blas/demo8_matrix_pinv.cpp
@@ -0,0 +1,92 @@
+// g++ -o demo_matrix demo_matrix.cpp -L/usr/local/lib -lopenblas
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <time.h>
+#include <sys/time.h>
+
+#include <cblas.h>
+#include <lapacke.h>
+#include <complex>
+
+#undef debug
+
+void affiche_matrice(std::complex<double>*mat,int x,int y)
+{int l,m;
+  for (m=0;m<x;m++) 
+   {for (l=0;l<y;l++)
+       printf("(%.5lf)+j*(%.5lf) ",real(mat[l*x+m]),imag(mat[l*x+m]));
+    printf(" ;\n");
+   }
+  printf("\n");
+}
+
+int main(int argc, char *argv[])
+{ int nobs=2000;
+  int nlag=15;
+  const int N=2*nlag+1;
+  int l,m,info;
+  int *IPIV;
+  int LWORK = N*N;
+  std::complex<double> *WORK;
+  std::complex<double> *host_mem,*host_code,*host_val,*host_res,*host_out,*host_final;
+  std::complex<double> alpha,beta,pwr; 
+  host_val=(std::complex<double>*)malloc(sizeof(std::complex<double>) * nobs);
+  alpha=1.;beta=0.;
+//https://www.netlib.org/lapack/explore-html/d1/d54/group__double__blas__level3_gaeda3cbd99c8fb834a60a6412878226e1.html
+  host_code=(std::complex<double>*)malloc(sizeof(std::complex<double>)*nobs);
+  host_res=(std::complex<double>*)malloc(sizeof(std::complex<double>)*(nlag*2+1)*(nlag*2+1));
+  host_mem=(std::complex<double>*)malloc(sizeof(std::complex<double>)*(2*nlag+1)*nobs);
+  host_out=(std::complex<double>*)malloc(sizeof(std::complex<double>)*(2*nlag+1)*nobs);
+  host_final=(std::complex<double>*)malloc(sizeof(std::complex<double>)*(2*nlag+1));
+  WORK=(std::complex<double>*)malloc(sizeof(std::complex<double>) * LWORK);
+  IPIV=(int*)malloc(sizeof(int) * N);
+  for (m=0;m<nobs;m++) 
+      {host_val[m].real(0); // (double)(random()/pow(2,31))-0.5);
+       host_val[m].imag(0); //(double)(random()/pow(2,31))-0.5);
+       host_code[m].real((double)(random()/pow(2,31))-0.5);
+       host_code[m].imag((double)(random()/pow(2,31))-0.5);
+      }
+  printf("\n");
+  for (m=0;m<nobs-5;m++)          // time shifted copies of the code
+      {host_val[m+2]+=0.5*host_code[m]; // m+4 = ...
+       host_val[m+5]+=1.2*host_code[m];
+       host_val[m]+=0.3*host_code[m+3];
+      }
+#ifdef debug
+  printf("a=[\n");
+  for (m=0;m<nobs;m++) printf("(%.2lf)+j*(%.2lf) ",real(host_val[m]),imag(host_val[m]));
+  printf("];\n");
+#endif
+  for (m=0;m<(2*nlag+1)*nobs;m++) host_mem[m]=0.;
+  for (l=-nlag;l<=nlag;l++)
+    for (m=0;m<nobs-(l+nlag);m++)
+       if (l<0) host_mem[(m)+nobs*(l+nlag)]=host_code[m-l];
+          else  host_mem[(m+l)+nobs*(l+nlag)]=host_code[m];
+#ifdef debug
+//  printf("b=[\n"); for (m=0;m<nlag*nobs;m++) printf("(%.2lf)+j*(%.2lf) ",real(host_mem[(m)]),imag(host_mem[(m)]));
+//  printf("];\n");
+  printf("b=[\n");
+  affiche_matrice(host_mem,nobs,2*nlag+1);
+  printf("];\n");
+#endif
+  cblas_zgemm(CblasColMajor, CblasConjTrans, CblasNoTrans, 2*nlag+1, 2*nlag+1, nobs, &alpha, host_mem, nobs, host_mem, nobs, &beta, host_res, 2*nlag+1 ); 
+  // C := alpha*op( A )*op( B ) + beta*C,
+  // alpha and beta are scalars, and A, B and C are matrices, with op( A )
+  // an m by k matrix,  op( B )  a  k by n matrix and  C an m by n matrix.
+  // (T/N,T/N,m,n,k,alpha, A,m si N ou k si T, B, k si N ou n si T, beta, C, m)
+  zgetrf_(&N,&N,reinterpret_cast <__complex__ double*>(host_res),&N,IPIV,&info); // LU decomposition: modifies input to output
+  if (info!=0) printf("error: info = %d\n",info);
+  zgetri_(&N,reinterpret_cast <__complex__ double*>(host_res),&N,IPIV,reinterpret_cast <__complex__ double*>(WORK),&LWORK,&info); // inverse
+  if (info!=0) printf("error: info = %d\n",info);
+#ifdef debug
+  affiche_matrice(host_res,(nlag*2+1), 2*nlag+1);
+#endif
+  cblas_zgemm(CblasColMajor, CblasNoTrans, CblasNoTrans, nobs, 2*nlag+1, 2*nlag+1, &alpha, host_mem, nobs, host_res, 2*nlag+1, &beta, host_out, nobs ); 
+#ifdef debug
+  affiche_matrice(host_out,nobs, 2*nlag+1);
+#endif
+  cblas_zgemm(CblasColMajor, CblasConjTrans, CblasNoTrans, 1, 2*nlag+1, nobs, &alpha, host_val, nobs, host_out, nobs, &beta, host_final, 1 ); 
+  affiche_matrice(host_final,1, 2*nlag+1);
+  printf("\n");
+}
