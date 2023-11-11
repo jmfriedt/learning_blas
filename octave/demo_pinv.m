ce6f4320b86e21175aa84ed7beaaa6a54d49ebb7
Jean-Michel Friedt <friedtj@free.fr>
2023-11-11 11:32:48 +0100

    weights in octave demo

--- octave/demo_pinv.m
+++ octave/demo_pinv.m
@@ -4,18 +4,20 @@ x=rand(P,1)-0.5;
 signal=rand(P,1)-0.5;
 xavant=x;
 for p=[10 20]
-  x(p:P)=x(p:P)+signal(1:end-p+1);
+  x(p:P)=x(p:P)+signal(1:end-p+1)*p/15;
 end
 Nlag=44;
 matrice=zeros(Nlag+1,length(x));
 for N=0:Nlag
-    matrice(N+1,N+1:P)=signal(1:end-N);
+  matrice(N+1,N+1:P)=signal(1:end-N);
 end
 poidspinv=x'*pinv(matrice);
 poids=x'*(matrice'*inv(matrice*matrice'));
 size(matrice*matrice')
 subplot(211)
 plot(poids)
+ylabel('weight (no unit)')
+xlabel('delay (samples)')
 subplot(212)
 plot(x')
 hold on
@@ -24,3 +26,6 @@ plot(xavant'-(x'-poidspinv*matrice))
 std(x')
 std(xavant')
 std(xavant'-(x'-poidspinv*matrice))
+xlabel('time (samples)')
+ylabel('signal (a.u.)')
+legend('signal+interference','signal','residue after cleaning')
